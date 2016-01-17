8a4db02016c400c78cdeeea326c21db40825b01a
bagofmice <bagofmice@gmail.com>
2016-01-17 10:43:36 -0800

    This still deploys to device. Initial commit

---
+++ X/OS/Windows 10 Http Client1/Converters/DateTimeToElapsedHoursConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Windows_10_Http_Client1.Converters
+{
+    public class DateTimeToElapsedHoursConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dateTime = (DateTime)value;
+
+            var elapsedHours = (DateTime.UtcNow - dateTime).Hours;
+
+            return elapsedHours + " hours ago";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
